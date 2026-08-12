9102de05436b0a2abcc2a31236d67f27edbf1318
Steve Moraco <steve@lander.media>
2026-08-11 21:43:44 -0600

    🧯 Simplify public Lake project for direct replay

--- verification/rh_run17_block/lakefile.lean
+++ verification/rh_run17_block/lakefile.lean
@@ -1,11 +1,7 @@
 import Lake
 open Lake DSL
 
-package «rh-run17-block» where
-  name := "rh-run17-block"
+package «rh-run17-block»
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.31.0"
-
-lean_lib «RHDyadicBlockFinite» where
-  roots := #[`RHDyadicBlockFinite]
