b24a3ff93647c85334ec9d3a457f4244c2b9c99c
Steve Moraco <steve@lander.media>
2026-08-13 09:09:00 -0600

    Use current pinned Mathlib for RH auxiliary replay

--- verification/rh-fayed-dependent-auxiliary/lakefile.lean
+++ verification/rh-fayed-dependent-auxiliary/lakefile.lean
@@ -5,4 +5,4 @@ package «rh-fayed-dependent-auxiliary» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @
-    "9c9a8e0a102b73c62db72c0e12a38522b2a1bd1c"
+    "v4.32.1"
