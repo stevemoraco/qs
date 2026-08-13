e55ce8d0127a8c3eea06dc6c138627d44a132486
Steve Moraco <steve@lander.media>
2026-08-13 07:26:38 -0600

    PNP verifier: add exact upstream axiom report

---
+++ formal/pnp-pedigree-axiom-audit/PedigreeAxiomReport.lean
@@ -0,0 +1,7 @@
+import MembershipProject.Core.N_PEqualsNP
+
+#print MembershipProject.Core.P_equals_NP
+#print MembershipProject.Core.STSP_in_P
+#print MembershipProject.Core.SAT_in_P
+#print MembershipProject.Core.An
+#print axioms MembershipProject.Core.p_equals_np
