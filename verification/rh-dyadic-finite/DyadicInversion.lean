d4283af9c4d8f2ecd4a5e6b4139ab80dad82587f
Steve Moraco <steve@lander.media>
2026-08-13 06:33:34 -0600

    Repair finite dyadic inversion parser notation

--- verification/rh-dyadic-finite/DyadicInversion.lean
+++ verification/rh-dyadic-finite/DyadicInversion.lean
@@ -10,8 +10,8 @@ theorem finite_inversion
       m (k + 1) = h k - 3 * h (k + 1) + 2 * h (k + 2)) :
     ∀ N : ℕ,
       h 0 =
-        (∑ k in Finset.range N,
-          (((2 : ℝ) ^ (k + 1) - 1) * m (k + 1)))
+        Finset.sum (Finset.range N)
+          (fun k => (((2 : ℝ) ^ (k + 1) - 1) * m (k + 1)))
         + (((2 : ℝ) ^ (N + 1) - 1) * h N)
         - (((2 : ℝ) ^ (N + 1) - 2) * h (N + 1)) := by
   intro N
