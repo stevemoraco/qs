e6c2d019a8fff9c71104ddf9340fe74a551c3309
Steve Moraco <steve@lander.media>
2026-08-13 07:15:22 -0600

    Pin mathlib for PNP restriction averaging replay

---
+++ verification/pnp-restriction-averaging/lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package «pnp-restriction-averaging» where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @
+    "9c9a8e0a102b73c62db72c0e12a38522b2a1bd1c"
