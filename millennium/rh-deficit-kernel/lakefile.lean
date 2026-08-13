5467e600feadd2032502c86ab2f9e5a039eb51ec
Steve Moraco <steve@lander.media>
2026-08-13 01:44:52 -0600

    Replay RH deficit theorem on exact Lean 4.31 toolchain

--- millennium/rh-deficit-kernel/lakefile.lean
+++ millennium/rh-deficit-kernel/lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package rhDeficitKernel
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.19.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.31.0"
 
 @[default_target]
 lean_lib RHDeficitKernel where
