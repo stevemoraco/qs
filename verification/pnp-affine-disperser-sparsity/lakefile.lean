6a9930e858502066f339eb6b97288437e7185a48
Steve Moraco <steve@lander.media>
2026-08-14 01:46:17 -0600

    Pin Mathlib for affine sparsity replay

---
+++ verification/pnp-affine-disperser-sparsity/lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package «pnp-affine-disperser-sparsity» where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @
+    "520045ab14e26149ee970e2e617ca04b09bde5d6"
